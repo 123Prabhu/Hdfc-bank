90ba80f385302d7b3c95f6500716564603f5355a
peehoo <prabhukalyanipanda98@gmail.com>
2022-10-25 19:45:25 +0530

    adding file

---
+++ src/main/java/com/exam/examportal/ExamportalApplication.java
@@ -0,0 +1,23 @@
+
+package com.exam.examportal;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.love2code.ecommerceproject.EcommerceProjectApplication;
+@SpringBootApplication
+
+public class ExamportalApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamportalApplication.class, args);
+	}
+	 
+ }
+//@SpringBootApplication
+//public class  ExamportalApplication {
+//
+//	public static void main(String[] args) {
+//		
+//	}
+
+
